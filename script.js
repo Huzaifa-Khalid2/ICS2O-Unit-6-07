072112b5e2a3a8e0d1ed2a962771d00583a33d23
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-08 16:00:59 +0000

    changed a lot

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.getElementById('my-btn').addEventListener('click', determiner)
 function determiner() {
-  let num1 = document.getElementById('my-text').value
-  num1= parseInt(num1)
-  let num2 = document.getElementById('my-text2').value
+  let n1 = document.getElementById('my-text').value
+  n1= parseInt(n1)
+  let n2 = document.getElementById('my-text2').value
+  n2= parseInt(n2)
   let ans=0
   ans = parseInt(ans)  
-  for (num2= document.getElementById('my-text').value num2 > 0; num2--) {
-  ans=ans+num1
+  for (n2 > 0; n2++;) {
+  ans=ans+n1
 }
-if (num2===0) {
+if (n2===0) {
   alert (ans)
 }
 }
