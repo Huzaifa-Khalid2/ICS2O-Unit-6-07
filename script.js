5fd05d6fec57d96ca1833eea740d34db5bb85a8a
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-23 21:57:26 +0000

    ssdfeg

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.getElementById('my-btn').addEventListener('click', determiner)
-function determiner() {
+function determiner() { 
   let n1 = document.getElementById('my-text').value
   n1= parseInt(n1)
   let n2 = document.getElementById('my-text2').value
   n2= parseInt(n2)
   let ans=0
   ans = parseInt(ans)  
-  for (n2 > 0; n2++;) {
+  for (n2, n2 <= 0; n2--;) {
   ans=ans+n1
 }
-if (n2===0) {
   alert (ans)
-}
 }
