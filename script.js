625b0cd90342241a6bf6cdea7423276469fd7702
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-23 22:20:16 +0000

    we got em

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('my-btn').addEventListener('click', determiner)
-function determiner() { 
+ function determiner() {
   let n1 = document.getElementById('my-text').value
-  n1= parseInt(n1)
+  n1 = parseInt(n1)
   let n2 = document.getElementById('my-text2').value
-  n2= parseInt(n2)
-  let ans=0
-  ans = parseInt(ans)  
+  n2 = parseInt(n2)
+  let ans = 0
+  ans = parseInt(ans)
   for (n2, n2 <= 0; n2--;) {
-  ans=ans+n1
-}
-  alert (ans)
+    ans = ans + n1
+  }
+  alert(ans)
 }
